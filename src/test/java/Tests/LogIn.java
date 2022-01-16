1a6dd0f2c80ec664bebcffdf366913745fa4df61
AleksandraStamenic <alexandravuc@icloud.com>
2022-01-16 13:29:42 +0100

    login with invalid email and pass

--- src/test/java/Tests/LogIn.java
+++ src/test/java/Tests/LogIn.java
@@ -1,7 +1,11 @@
 package Tests;
 
 import Pages.HomePage;
+import Pages.UserIcon;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LogIn extends BaseTest{
@@ -12,7 +16,22 @@ public class LogIn extends BaseTest{
         HomePage mtsMeni=new HomePage(driver);
         mtsMeni.clickonUserIcon();
 
-
-
     }
-}
+     @Test
+
+     public void InvalidLogin(){
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://moj.mts.rs/Prijava-b2c");
+
+        driver.findElementByXPath("//*[@id=\"selfCareComponent\"]/section/section/div[3]/form[1]/label[1]/input").sendKeys("alex@gmail.com");
+        driver.findElementByXPath("//*[@id=\"selfCareComponent\"]/section/section/div[3]/form[1]/label[2]").sendKeys("Password");
+        driver.findElementByXPath("//*[@id=\"pass-input16\"]/span").click();
+        driver.findElementByXPath("//*[@id=\"selfCareComponent\"]/section/section/div[3]/form[1]/div/button").click();
+        String actual_error=driver.findElementByXPath("//*[@id=\"origamiPageWrap\"]/div[6]").getText();
+        String expected_error="Uneti podaci nisu ispravni.";
+        if (actual_error.equals(expected_error)){
+            System.out.println("TRUE");
+        }
+
+
+     }}
